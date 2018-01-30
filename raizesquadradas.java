e298e94a18e4c385b911f9b143b13acba4c718cd
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2018-01-30 11:04:30 +0000

    Add files via upload

---
+++ raizesquadradas.java
@@ -0,0 +1,19 @@
+package raizesquadradas;
+import java.util.Scanner;
+public class raizesquadradas {
+static void raizquadrada (int Linf , int Lsup)
+{
+Scanner dados = new Scanner (System.in);	
+System.out.println("Qual o limite inferior?");	
+Linf= dados.nextInt();	
+System.out.println("Qual o limite superior?");	
+Lsup= dados.nextInt();	
+for (int I=Linf ; I<Lsup ; I++)	
+System.out.println(String.format("%5.1f",Math.sqrt(I)));
+}	
+public static void main(String[] args) {
+raizquadrada(0,0);	
+}
+}
+
+.
